7b0f2e93651b1316b688a4ef26c23a0fc3ed5bcd
nhatbinhtran2006-collab <nhatbinh.tran2006@gmail.com>
2026-02-03 01:08:09 +0700

    up

--- src/api/schemas/employee.py
+++ src/api/schemas/employee.py
@@ -1,14 +1,34 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
+
 
 class EmployeeRequestSchema(Schema):
-    employee_name = fields.Str(required=True)
-    owner_id = fields.Int(required=True) # ID của BusinessOwner
-    role = fields.Str()
-    active_status = fields.Bool()
+    employee_name = fields.Str(
+        required=True,
+        validate=validate.Length(min=2, max=100)
+    )
+
+    owner_id = fields.Int(required=True)
+
+    role = fields.Str(
+        validate=validate.OneOf([
+            "manager",
+            "cashier",
+            "staff",
+            "accountant"
+        ])
+    )
+
+    active_status = fields.Bool(missing=True)
+
 
 class EmployeeResponseSchema(Schema):
     employee_id = fields.Int()
+
     employee_name = fields.Str()
     owner_id = fields.Int()
+
     role = fields.Str()
     active_status = fields.Bool()
+
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
