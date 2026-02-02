3a19f12958e0ce9c9abcf666fd5ff3c11d3f19e8
nhatbinhtran2006-collab <nhatbinh.tran2006@gmail.com>
2026-02-03 00:38:30 +0700

    up

--- src/api/schemas/account_report.py
+++ src/api/schemas/account_report.py
@@ -1,10 +1,20 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
+
 
 class AccountReportRequestSchema(Schema):
     owner_id = fields.Int(required=True)
-    report_type = fields.Str(required=True)
+    report_type = fields.Str(
+        required=True,
+        validate=validate.OneOf([
+            "daily",
+            "weekly",
+            "monthly",
+            "yearly"
+        ])
+    )
     reporting_period = fields.Str(required=True)
     generated_date = fields.Date(required=True)
+
 
 class AccountReportResponseSchema(Schema):
     report_id = fields.Int()
@@ -12,3 +22,9 @@ class AccountReportResponseSchema(Schema):
     report_type = fields.Str()
     reporting_period = fields.Str()
     generated_date = fields.Date()
+
+    total_income = fields.Float()
+    total_expense = fields.Float()
+    profit = fields.Float()
+
+    status = fields.Str()
